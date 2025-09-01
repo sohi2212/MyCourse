6a8b165ecb1c7f8261b649a3fe37998136fc202e
Dima <sohi02@bk.ru>
2025-09-01 12:54:44 +0300

    Изменил формулу

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const randDice = (dice) => {
     const dices = [
-        { dice: "D4", max: 4 },
-        { dice: "D6", max: 6 },
-        { dice: "D8", max: 8 },
-        { dice: "D10", max: 10 },
-        { dice: "D12", max: 12 },
-        { dice: "D16", max: 16 },
-        { dice: "D20", max: 20 },
+        { dice: "D4", max: 4, min: 1 },
+        { dice: "D6", max: 6, min: 1 },
+        { dice: "D8", max: 8, min: 1 },
+        { dice: "D10", max: 10, min: 1 },
+        { dice: "D12", max: 12, min: 1 },
+        { dice: "D16", max: 16, min: 1 },
+        { dice: "D20", max: 20, min: 1 },
     ]
 
     const selectedDice = dices.find(d => d.dice === dice)
@@ -15,7 +15,7 @@ const randDice = (dice) => {
     }
 
     const rand = Math.random()
-    return Math.floor(rand * (selectedDice.max - 0 + 1) + 0)
+    return Math.floor(rand * (selectedDice.max - selectedDice.min + 1) + selectedDice.min)
 }
 
 const dice = prompt('Введите название dice')

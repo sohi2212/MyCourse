18cb34df2e122da3656998decc924d82cf998c76
Dmitry <sohi02@bk.ru>
2025-09-06 16:56:53 +0300

    complete

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-const validatorAge = (bd) => {
-    const now = new Date()
-    const isValid = new Date(now)
-    isValid.setFullYear(now.getFullYear() - 14)
-    if (Number(new Date(bd)) <= Number(isValid)) {
-        return true
-    } else {
-        return false
-    }
+const timerHtml = document.querySelector('.time')
+const endOfYear = new Date(new Date().getFullYear(), 11, 31, 23, 59, 59)
+
+const timeToYearEnd = () => {
+    const interval = setInterval(() => {
+        const now = new Date()
+
+        const months = (endOfYear.getMonth() - now.getMonth())
+        const days = (endOfYear.getDate() - now.getDate())
+        const hours = (endOfYear.getHours() - now.getHours())
+        const minutes = (endOfYear.getMinutes() - now.getMinutes())
+        const seconds = (endOfYear.getSeconds() - now.getSeconds())
+
+        timerHtml.innerHTML = `${months} месяцев, ${days} дней, ${hours} часов, ${minutes} минут, ${seconds} секунд`
+    }, 1000)
 }
+
+timeToYearEnd()

cfe5e595e3a1ab7b80dcf2b9a0e2a42099d291f6
Dmitry <sohi02@bk.ru>
2025-09-06 12:44:20 +0300

    r

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ const validatorAge = (bd) => {
     const now = new Date()
     const isValid = new Date(now)
     isValid.setFullYear(now.getFullYear() - 14)
-    if (Number(new Date(bd)) >= Number(isValid)) {
+    if (Number(new Date(bd)) <= Number(isValid)) {
         return true
     } else {
         return false
     }
 }
+
+console.log(validatorAge('2023-01-02'))
